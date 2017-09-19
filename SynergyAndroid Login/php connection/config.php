72e32cecd73201dedc2bd87d18be48f9f4aa09be
RileyBrown <riley.brown@my.jcu.edu.au>
2017-09-19 18:29:29 +1000

    Android Login version
    Worked on developing the android login version. :)

---
+++ SynergyAndroid Login/php connection/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Riley
+ * Date: 18/09/2017
+ * Time: 8:37 PM
+ */
+
+define('hostname', 'localhost');
+define('username', 'root');
+define('password', '');
+define('db_name', 'registerandroid');
+
+
+?>
